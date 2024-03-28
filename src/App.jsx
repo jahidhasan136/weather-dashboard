6dc1e7b5df452bffe3bfb39bdd43621eecc63bc8
Jahid Hasan <jh3495689@gmail.com>
2024-03-28 07:48:26 +0600

    implement favourites integrations

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import "./App.css";
 import Header from "./components/header/Header";
 import WeatherBoard from "./components/weather/WeatherBoard";
+import { FavouriteProvider } from "./provider";
 import WeatherProvider from "./provider/WeatherProvider";
 
 function App() {
   return (
     <WeatherProvider>
-      <div className="grid place-items-center h-screen">
-        <Header />
-        <main>
-          <section>
-            <WeatherBoard />
-          </section>
-        </main>
-      </div>
+      <FavouriteProvider>
+        <div className="grid place-items-center h-screen">
+          <Header />
+          <main>
+            <section>
+              <WeatherBoard />
+            </section>
+          </main>
+        </div>
+      </FavouriteProvider>
     </WeatherProvider>
   );
 }
--- src/components/weather/AddToFavourite.jsx
+++ src/components/weather/AddToFavourite.jsx
@@ -1,12 +1,33 @@
+import { useContext, useState } from "react";
+import redFavouriteIcon from "../../assets/heart-red.svg";
 import favouriteIcon from "../../assets/heart.svg";
+import { FavouriteContext, WeatherContext } from "../../context";
 
 const AddToFavourite = () => {
+  const { addToFavourites, removeFromFavourites, favourites } =
+    useContext(FavouriteContext);
+  const { weatherData } = useContext(WeatherContext);
+  const [isFavourite, toogleFavourite] = useState(false);
+  const { latitude, longitude, location } = weatherData;
+  // console.log(latitude, longitude, location);
+  function handleFavourites() {
+    const found = favourites.find((fav) => fav.location === location);
+    if (!found) {
+      addToFavourites(latitude, longitude, location);
+    } else {
+      removeFromFavourites(location);
+    }
+    toogleFavourite(!isFavourite);
+  }
   return (
     <div className="md:col-span-2">
       <div className="flex items-center justify-end space-x-6">
-        <button className="text-sm md:text-base inline-flex items-center space-x-2 px-3 py-1.5 rounded-md bg-[#C5C5C54D]">
+        <button
+          className="text-sm md:text-base inline-flex items-center space-x-2 px-3 py-1.5 rounded-md bg-[#C5C5C54D]"
+          onClick={handleFavourites}
+        >
           <span>Add to Favourite</span>
-          <img src={favouriteIcon} alt="" />
+          <img src={isFavourite ? redFavouriteIcon : favouriteIcon} alt="" />
         </button>
       </div>
     </div>
